a0aa75cb415732042d83fa3773ff1b96f5475815
Julien <julien.rougeron@gmail.com>
2025-11-07 20:40:47 +0000

    Last clean up - setting the correct url for fetching data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => (
   <div className='w-full flex bg-cream justify-center p-2'>
     <div className='flex max-w-7xl w-full justify-between'>
-      <img src='/public/logo.png' alt='Logo' className='h-10' />
+      <img src='/logo.png' alt='Logo' className='h-10' />
 
       <div className='flex items-center font-semibold'>👤 Julien</div>
     </div>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -10,17 +10,16 @@ export const useData = () => {
     setLoading(true)
 
     try {
-      // Commented out for local development
-      // const response = await fetch(
-      //   'https://strapi.teetsh.com/api/programmations/d7tfhdcchm1bom0df2z6s8zv',
-      //   {
-      //     headers: {
-      //       Authorization:
-      //         'Bearer 2dba999015fbc84a7d1f6dd11b85c4f72bec98b28c9cecac6986780bcdd8e0ee42827a263ed0b54ee5f1b9910dcda1cbcc4368edc2862b1b1bd8d0339967de45556d4e0d0da6c9c5d28d790fb4f3cd8115ee27e1cb95dc1ea705604aec863197857e8b0af3f4639fff8788cf6f5ba2f20b4c23048956e3891ab60aa703c5729f',
-      //     },
-      //   },
-      // )
-      const response = await fetch('/src/assets/data.json')
+      const response = await fetch(
+        'https://strapi.teetsh.com/api/programmations/d7tfhdcchm1bom0df2z6s8zv',
+        {
+          headers: {
+            Authorization:
+              'Bearer 2dba999015fbc84a7d1f6dd11b85c4f72bec98b28c9cecac6986780bcdd8e0ee42827a263ed0b54ee5f1b9910dcda1cbcc4368edc2862b1b1bd8d0339967de45556d4e0d0da6c9c5d28d790fb4f3cd8115ee27e1cb95dc1ea705604aec863197857e8b0af3f4639fff8788cf6f5ba2f20b4c23048956e3891ab60aa703c5729f',
+          },
+        },
+      )
+
       const data = await response.json()
       setData(data.data)
     } catch (error) {
